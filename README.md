c169373747e02bb15c6a313f29e4f94e5fe40b98
ChidiChuks <chidiebere_chukwuma@yahoo.com>
2019-09-15 12:46:22 +0100

    notification activity and react set-up

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # React-Components
 A fully functional timeline feed with likes and comments, a notification feed with real-time push and a status update form with support for emojis, images, links, mentions, and other rich content
+
+## Details
+- Building an app using React and Stream’s react-activity-feed, a library full of components for building activity feeds.
+
+## Set Up
+- `npx create-react-app react-feeds-example`
+- `cd react-feeds-example`
+- `npm install react-activity-feed`
+- `npm start`
+
+## To find out more about deployment
+- `https://bit.ly/CRA-deploy`
---
+++ react-feeds-example/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StreamApp, NotificationDropdown, FlatFeed } from 'react-activity-feed';
+import 'react-activity-feed/dist/index.css';
+
+
+class App extends React.Component {
+    render() {
+        return ( <
+            StreamApp apiKey = "du8he7epvp94"
+            appId = "45206"
+            token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMDcwY2RlMmQtOTViZC00YzE0LWFiOGQtOGY4OGZiNjhlMWZkIn0.43jupEoJVPPLysRPpHj5Xj04MUkq0LksWgVngj3YokM" >
+            <
+            NotificationDropdown notify / >
+            <
+            FlatFeed notify / >
+            <
+            /StreamApp>
+        );
+    }
+}
+
+export default App;
